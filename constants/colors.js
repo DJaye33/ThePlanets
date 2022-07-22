30599d1d51fc5ba755d105bec68fec9219e3317e
DJaye33 <dlouis3386@gmail.com>
2022-07-22 17:01:21 -0400

    add rootview background color

---
+++ constants/colors.js
@@ -0,0 +1,14 @@
+export const colors = {
+  white: "#fff",
+  darkBlue: "#070724",
+  darkGrey: "#38384f",
+  lightGrey: "#838391",
+  aqua: "#419ebb",
+  gold: "#eda249",
+  purple: "#6f2ed6",
+  orangeRed: "#d14c32",
+  red: "#d83a44",
+  orange: "#cd5120",
+  teal: "#1ec2a4",
+  blue: "#2d68f0",
+};
